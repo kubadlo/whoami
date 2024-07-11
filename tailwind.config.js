889428e093aa0f3742811a3da7f8301e24d195be
Jakub Leško <mail@jakublesko.com>
2024-07-11 08:16:58 +0200

    feat: remove custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,7 @@
-import { fontFamily } from 'tailwindcss/defaultTheme';
-
 export default {
   content: ['index.html'],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', ...fontFamily.sans]
-      }
-    }
+    extend: {}
   },
   plugins: []
 };
