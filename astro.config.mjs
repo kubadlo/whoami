280e27b261ca3b3328112d963c576a6f7d9baddc
Jakub Leško <mail@jakublesko.com>
2025-03-06 09:44:45 +0100

    feat: add tailwindcss and sitemap integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,12 @@
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+
 import { defineConfig } from "astro/config";
 
-export default defineConfig({});
+export default defineConfig({
+  site: "https://jakublesko.com",
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
